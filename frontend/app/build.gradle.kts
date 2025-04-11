2e89a0a7a6677fd0fff05f1351f7c69df1cff37f
Aparimit <aparimit2003@gmail.com>
2025-04-12 01:47:28 +0530

    dependencies imported

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
 	alias(libs.plugins.android.application)
 	alias(libs.plugins.kotlin.android)
 	alias(libs.plugins.kotlin.compose)
+
+//	Firebase
+	alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -56,4 +59,16 @@ dependencies {
 	androidTestImplementation(libs.androidx.ui.test.junit4)
 	debugImplementation(libs.androidx.ui.tooling)
 	debugImplementation(libs.androidx.ui.test.manifest)
+
+//	Firebase SDK Dependencies
+	implementation(libs.firebase.auth)
+	implementation(libs.androidx.credentials)
+	implementation(libs.androidx.credentials.play.services.auth)
+	implementation(libs.googleid)
+	implementation(libs.firebase.database)
+	implementation(libs.firebase.firestore)
+
+//	Coroutines Dependencies
+	implementation(libs.kotlinx.coroutines.core)
+	implementation(libs.kotlinx.coroutines.android)
 }
